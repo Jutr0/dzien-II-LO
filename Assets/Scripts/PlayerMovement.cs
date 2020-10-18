74e982c8b15d16ce5ed32cb14ea2e140c3537d75
Jutr0 <67229805+Jutr0@users.noreply.github.com>
2020-10-18 18:35:07 +0200

    Pierwszy commit, dodanie Assetsow

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D playerRb;
+    public float moveSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical= Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        playerRb.MovePosition(playerRb.position + move * Time.fixedDeltaTime * moveSpeed);
+
+    }
+}
